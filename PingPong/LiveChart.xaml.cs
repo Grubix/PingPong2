e9f79bc90184a8201cf55942667ba29a5cdec94e
Grubix <krystian.borowicz@student.put.poznan.pl>
2021-01-06 00:02:30 +0100

    Add project files.

---
+++ PingPong/LiveChart.xaml.cs
@@ -0,0 +1,130 @@
+﻿using OxyPlot;
+using OxyPlot.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PingPong
+{
+    /// <summary>
+    /// Interaction logic for LiveChart.xaml
+    /// </summary>
+    public partial class LiveChart : UserControl {
+
+        private readonly Stopwatch stopWatch = new Stopwatch();
+
+        private long sample = 0;
+
+        private long deltaTime = 0;
+
+        public int RefreshDelay { get; set; } = 100;
+
+        public int MaxSamples { get; set; } = 5000;
+
+        public string YAxisTitle {
+            get {
+                return chart.Axes[1].Title;
+            }
+            set {
+                chart.Axes[1].Title = value;
+            }
+        }
+
+        public LiveChart() {
+            InitializeComponent();
+        }
+
+        public void AddSeries(string title, string name, bool visible) {
+            LineSeries series = new LineSeries {
+                Title = title
+            };
+
+            chart.Series.Add(series);
+            series.ItemsSource = new List<DataPoint>();
+
+            CheckBox checkbox = new CheckBox {
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
+            };
+
+            if (visible) {
+                checkbox.IsChecked = true;
+            } else {
+                series.Visibility = Visibility.Hidden;
+            }
+
+            if (visibleSeries.Children.Count != 0) {
+                checkbox.Margin = new Thickness(5, 0, 0, 0);
+            }
+
+            checkbox.Click += (s, e) => {
+                if ((bool)checkbox.IsChecked) {
+                    series.Visibility = Visibility.Visible;
+                } else {
+                    series.Visibility = Visibility.Hidden;
+                }
+            };
+
+            Label label = new Label {
+                Content = name
+            };
+
+            StackPanel panel = new StackPanel {
+                Orientation = Orientation.Horizontal,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
+            };
+
+            panel.Children.Add(checkbox);
+            panel.Children.Add(label);
+            visibleSeries.Children.Add(panel);
+        }
+
+        public void Update(double[] data) {
+            if (data.Length != chart.Series.Count) {
+                throw new ArgumentException();
+            }
+
+            stopWatch.Stop();
+
+            deltaTime += stopWatch.ElapsedMilliseconds;
+
+            stopWatch.Reset();
+            stopWatch.Start();
+
+            if (deltaTime < RefreshDelay) {
+                sample++;
+                return;
+            } else {
+                deltaTime = 0;
+            }
+
+            for (int i = 0; i < data.Length; i++) {
+                ((List<DataPoint>)chart.Series[i].ItemsSource).Add(new DataPoint(sample, data[i]));
+            }
+
+            Dispatcher.Invoke(() => chart.InvalidatePlot(true));
+
+            sample++;
+        }
+
+        public void Clear() {
+            foreach (var series in chart.Series) {
+                ((List<DataPoint>)series.ItemsSource).Clear();
+            }
+        }
+
+        public void Freeze() {
+
+        }
+
+        public void Unfreeze() {
+
+        }
+
+        public void ResetZoom() {
+
+        }
+
+    }
+}
---
+++ PingPong/RobotPanel.xaml.cs
@@ -0,0 +1,85 @@
+﻿using OxyPlot;
+using OxyPlot.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PingPong {
+    /// <summary>
+    /// Interaction logic for RobotPanel.xaml
+    /// </summary>
+    public partial class RobotPanel : UserControl {
+
+        private bool isPlotFrozen = false;
+
+        public RobotPanel() {
+            InitializeComponent();
+            InitializePositionChart();
+            InitializeVelocityChart();
+            InitializeAccelerationChart();
+            InitializePositionErrorChart();
+
+            Task.Run(() => {
+                for (int i = 0; i < 20000; i++) {
+                    Thread.Sleep(4);
+
+                    if (isPlotFrozen) {
+                        continue;
+                    }
+
+                    positionChart.Update(new double[] {
+                        Math.Sin(i / 250.0) / 6.0,
+                        Math.Cos(i / 250.0) / 5.0,
+                        Math.Sin(i / 500.0) / 4.0,
+                        Math.Cos(i / 500.0) / 3.0,
+                        Math.Sin(i / 750.0) / 2.0,
+                        Math.Cos(i / 750.0) / 1.0
+                    });
+                }
+            });
+        }
+
+        private void InitializePositionChart() {
+            positionChart.YAxisTitle = "Position (actual)";
+            positionChart.AddSeries("Position X [mm]", "X", true);
+            positionChart.AddSeries("Position Y [mm]", "Y", true);
+            positionChart.AddSeries("Position Z [mm]", "Z", true);
+            positionChart.AddSeries("Position A [deg]", "A", false);
+            positionChart.AddSeries("Position B [deg]", "B", false);
+            positionChart.AddSeries("Position C [deg]", "C", false);
+        }
+
+        private void InitializePositionErrorChart() {
+            positionErrorChart.YAxisTitle = "Position error";
+            positionErrorChart.AddSeries("Error X [mm]", "X", true);
+            positionErrorChart.AddSeries("Error Y [mm]", "Y", true);
+            positionErrorChart.AddSeries("Error Z [mm]", "Z", true);
+            positionErrorChart.AddSeries("Error A [deg]", "A", false);
+            positionErrorChart.AddSeries("Error B [deg]", "B", false);
+            positionErrorChart.AddSeries("Error C [deg]", "C", false);
+        }
+
+        private void InitializeVelocityChart() {
+            velocityChart.YAxisTitle = "Velocity (theoretical)";
+            velocityChart.AddSeries("Velocity X [mm/s]", "X", true);
+            velocityChart.AddSeries("Velocity Y [mm/s]", "Y", true);
+            velocityChart.AddSeries("Velocity Z [mm/s]", "Z", true);
+            velocityChart.AddSeries("Velocity A [deg/s]", "A", false);
+            velocityChart.AddSeries("Velocity B [deg/s]", "B", false);
+            velocityChart.AddSeries("Velocity C [deg/s]", "C", false);
+        }
+
+        private void InitializeAccelerationChart() {
+            accelerationChart.YAxisTitle = "Acceleration (theoretical)";
+            accelerationChart.AddSeries("Acceleration X [mm/s]", "X", true);
+            accelerationChart.AddSeries("Acceleration Y [mm/s]", "Y", true);
+            accelerationChart.AddSeries("Acceleration Z [mm/s]", "Z", true);
+            accelerationChart.AddSeries("Acceleration A [deg/s]", "A", false);
+            accelerationChart.AddSeries("Acceleration B [deg/s]", "B", false);
+            accelerationChart.AddSeries("Acceleration C [deg/s]", "C", false);
+        }
+
+    }
+}
