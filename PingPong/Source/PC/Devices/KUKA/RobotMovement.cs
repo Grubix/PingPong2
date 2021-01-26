9c229a2677c4ac7c5437774dd1edead2191d245a
Grubix <krystian.borowicz@student.put.poznan.pl>
2021-01-26 14:23:46 +0100

    nowy generator

---
+++ PingPong/Source/PC/Devices/KUKA/RobotMovement.cs
@@ -0,0 +1,17 @@
+﻿namespace PingPong.KUKA {
+    public class RobotMovement {
+
+        public RobotVector TargetPosition { get; }
+
+        public RobotVector TargetVelocity { get; }
+
+        public double TargetDuration { get; }
+
+        public RobotMovement(RobotVector targetPosition, RobotVector targetVelocity, double targetDuration) {
+            TargetPosition = targetPosition;
+            TargetVelocity = targetVelocity;
+            TargetDuration = targetDuration;
+        }
+
+    }
+}
